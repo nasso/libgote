ee6860f15f0446b59c7b7fa7cbf302c2a571ade7
nasso <nassomails@gmail.com>
2020-02-29 00:50:45 +0100

    feat: implement gt_app_run

--- src/app/run.c
+++ src/app/run.c
@@ -10,5 +10,14 @@
 
 bool gt_app_run(gt_app_t *self)
 {
-    return (false);
+    bool err = false;
+    gt_state_data_t state_data = {
+        .data = self->data,
+        .world = self->world,
+    };
+
+    err = gt_state_machine_start(self->states, &state_data);
+    while (!err && gt_state_machine_is_running(self->states))
+        err = gt_state_machine_update(self->states, &state_data);
+    return (err);
 }
